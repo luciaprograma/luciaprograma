0ebc47698c5e8482bcc914da1c73fc492b99dccd
luciaprograma <luciaprograma@gmail.com>
2025-04-12 21:47:06 -0300

    htmls upgrade

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
-document.querySelectorAll('a[href^="http"]').forEach(link => {
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="http"], a[href^="mailto:"], a[href^="tel:"]').forEach(link => {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'noopener noreferrer');
   });
   
+});
+
