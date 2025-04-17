304d069b46836397f0a472313481951d2d6dd802
luciaprograma <luciaprograma@gmail.com>
2025-04-17 19:54:01 -0300

    spelling errors & cv upgrades

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
 
-  document.querySelectorAll('a[href^="http"], a[href^="mailto:"], a[href^="tel:"], a[href^="cv/"]').forEach(link => {
+  document.querySelectorAll('a[href^="http"], a[href^="mailto:"], a[href^="tel:"] ').forEach(link => {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'noopener noreferrer');
   });
